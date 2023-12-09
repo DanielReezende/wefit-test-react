f7638771621e83ccb9af0f7b6c2dfc69844989ee
Daniel Rezende <daniel.zende@gmail.com>
2023-12-09 00:28:31 -0300

    feat: Creation of the function to format the price in BRL format

--- src/components/Card/CardPrice.tsx
+++ src/components/Card/CardPrice.tsx
@@ -1,18 +1,20 @@
-import { useCallback } from "react";
+
+
 import { Price } from "./styles";
+import { useCallback } from "react";
 
 interface Props {
   price: number;
 }
 
 export function CardPrice({ price }: Props) {
-  
-  const formatPrice = useCallback((value: number) => {
-    return value.toLocaleString("pt-br", {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    });
-  }, []);
+  const formatPrice = useCallback(
+    (value: number): string =>
+      Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(
+        value
+      ),
+    []
+  );
 
   return <Price>{formatPrice(price)}</Price>;
 }
