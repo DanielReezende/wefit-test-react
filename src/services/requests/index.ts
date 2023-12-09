bdcc1001ac5a8dae1098f7749b7442c8d8f1da20
Daniel Rezende <daniel.zende@gmail.com>
2023-12-09 02:43:05 -0300

    feat: Creating a request to get information from just one movie

--- src/services/requests/index.ts
+++ src/services/requests/index.ts
@@ -6,3 +6,9 @@ export const getProducts = async () => {
 
   return data;
 };
+
+export const getProduct = async(id: number): Promise<Product> => {
+  const { data } = await api.get<Product>(`/products/${id}`);
+
+  return data;
+}
