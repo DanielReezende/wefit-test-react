c0164b455c60181fd284dc2a06ad0146d37ff730
Daniel Rezende <daniel.zende@gmail.com>
2023-12-09 07:48:31 -0300

    feat: Purchase screen completed successfully

--- src/pages/Cart/Success/index.tsx
+++ src/pages/Cart/Success/index.tsx
@@ -1,15 +1,20 @@
-
 // Component
 import { Button } from "@/components/Button";
 import { Card } from "@/components/Card";
 
-import SuccessAvatar from '@/assets/images/success.svg'
+import SuccessAvatar from "@/assets/images/success.svg";
 
 import { Container, Wrapper } from "./styles";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import { useCart } from "@/hooks/useCart";
 
 export function Success() {
   const navigate = useNavigate();
+  const { isPurchased } = useCart();
+
+  if (!isPurchased) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <Container>
@@ -17,9 +22,14 @@ export function Success() {
         <Wrapper>
           <h1>Compra realizada com sucesso!</h1>
 
-          <img src={SuccessAvatar} alt="Avatar de sucesso, para apresetação da finalização do pedido." />
+          <img
+            src={SuccessAvatar}
+            alt="Avatar de sucesso, para apresetação da finalização do pedido."
+          />
 
-          <Button.Container onClick={() => navigate("/")}>Voltar</Button.Container>
+          <Button.Container onClick={() => navigate("/")}>
+            Voltar
+          </Button.Container>
         </Wrapper>
       </Card.Container>
     </Container>
