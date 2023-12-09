05f5fad72f799733f712214f9151ff40755aa9d1
Daniel Rezende <daniel.zende@gmail.com>
2023-12-09 02:44:08 -0300

    refactor: Restructuring the page's css

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,27 +1,34 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const columns = (numberColumns: number) =>  `repeat(${numberColumns}, minmax(0, 1fr))`;
+const columns = (numberColumns: number) =>
+  `repeat(${numberColumns}, minmax(0, 1fr))`;
 
 export const Container = styled.main`
-  display: grid;
-  grid-template-columns: ${columns(1)};
-  gap: 1.6rem;
-
   padding: 1.6rem;
 
+  ul {
+    list-style: none;
 
+    display: grid;
+    grid-template-columns: ${columns(1)};
+    gap: 1.6rem;
+  }
 
   @media (min-width: 610px) {
-    grid-template-columns: ${columns(2)};
+    ul {
+      grid-template-columns: ${columns(2)};
+    }
   }
 
   @media (min-width: 910px) {
-    grid-template-columns: ${columns(3)};
+    ul {
+      grid-template-columns: ${columns(3)};
+    }
   }
 `;
 
 export const LoadingContainer = styled.main`
-  display:  flex;
+  display: flex;
   align-items: center;
   justify-content: center;
 
