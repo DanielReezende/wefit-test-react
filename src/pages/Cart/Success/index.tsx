a20aabd123366870a1cc6aaea2a765e22c00463c
Daniel Rezende <daniel.zende@gmail.com>
2023-12-09 01:33:58 -0300

    feat: completion of purchase success screen

--- src/pages/Cart/Success/index.tsx
+++ src/pages/Cart/Success/index.tsx
@@ -1,4 +1,27 @@
+
+// Component
+import { Button } from "@/components/Button";
+import { Card } from "@/components/Card";
+
+import SuccessAvatar from '@/assets/images/success.svg'
+
+import { Container, Wrapper } from "./styles";
+import { useNavigate } from "react-router-dom";
+
 export function Success() {
-return (<div>
-</div>);
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Card.Container>
+        <Wrapper>
+          <h1>Compra realizada com sucesso!</h1>
+
+          <img src={SuccessAvatar} alt="Avatar de sucesso, para apresetação da finalização do pedido." />
+
+          <Button.Container onClick={() => navigate("/")}>Voltar</Button.Container>
+        </Wrapper>
+      </Card.Container>
+    </Container>
+  );
 }
--- src/pages/Cart/Success/styles.ts
+++ src/pages/Cart/Success/styles.ts
@@ -1,5 +1,40 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.main`
-  
+  padding: 1.6rem;
+
+  width: 100%;
+`;
+
+export const Wrapper = styled.div`
+  width: 100%;
+
+  background-color: var(--white-900);
+  border-radius: var(--border-radius);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  text-align: center;
+
+  gap: 3.2rem;
+
+  padding: 5.4rem 1.3rem;
+
+  h1 {
+    font-size: 2rem;
+    line-height: 1.4;
+    color: var(--blue-900);
+  }
+
+  img {
+    width: 100%;
+    max-width: 295px;
+    object-fit: cover;
+  }
+
+  button {
+    max-width: 160px;
+  }
 `;
