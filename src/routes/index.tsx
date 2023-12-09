f84c31b25e90423846dbae9f694aedb9a969b601
Daniel Rezende <daniel.zende@gmail.com>
2023-12-09 00:24:51 -0300

    refactor: cart route configuration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,8 @@ import { DefaultLayout } from "@/layouts/default.layout";
 
 // Pages
 import { Home } from "@/pages/Home";
+import { Cart } from "@/pages/Cart";
+import { Success } from "@/pages/Cart/Success";
 
 
 export function Routers() {
@@ -12,7 +14,11 @@ export function Routers() {
     <Routes>
       <Route element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Home />} />
+
+        <Route path="/cart">
+          <Route path="" element={<Cart />} />
+          <Route path="success" element={<Success />} />
+        </Route>
       </Route>
     </Routes>
   );
