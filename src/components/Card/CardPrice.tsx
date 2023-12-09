31586cc030864143fe632e608e4ecad4a6513146
Daniel Rezende <daniel.zende@gmail.com>
2023-12-09 07:42:28 -0300

    refactor: Separation of the formatPrice function in the helper file

--- src/components/Card/CardPrice.tsx
+++ src/components/Card/CardPrice.tsx
@@ -1,20 +1,13 @@
-
+import { formatPrice } from "@/helpers/formatPrice";
 
 import { Price } from "./styles";
-import { useCallback } from "react";
+
 
 interface Props {
   price: number;
 }
 
 export function CardPrice({ price }: Props) {
-  const formatPrice = useCallback(
-    (value: number): string =>
-      Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(
-        value
-      ),
-    []
-  );
 
   return <Price>{formatPrice(price)}</Price>;
 }
