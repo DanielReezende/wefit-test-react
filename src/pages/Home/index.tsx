6496ad790a71a9647307c1e120cf65395a846caa
Daniel Rezende <daniel.zende@gmail.com>
2023-12-09 02:43:39 -0300

    feat: Integration with the useCart hook

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,11 +11,27 @@ import { Spinner } from "@/components/Spinner";
 // Requests
 import { getProducts } from "@/services/requests";
 
+// Custom Hooks
+import { useCart } from "@/hooks/useCart";
+
+// Types
+import { CartItemsAmount } from "@/@types/application";
+
 import { Container, LoadingContainer } from "./styles";
-import { Product } from "@/@types/application";
+
 
 export function Home() {
-  const { isLoading, data } = useQuery<Product[]>("moviesData", getProducts);
+  const { isLoading, data } = useQuery("moviesData", getProducts);
+  const { addProduct, cart } = useCart();
+
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount;
+    return sumAmount;
+  }, {} as CartItemsAmount);
+
+  function handleAddProduct(id: number) {
+    addProduct(id);
+  }
 
   if (isLoading) {
     return (
@@ -27,20 +43,28 @@ export function Home() {
 
   return (
     <Container>
-      {data?.map((product) => (
-        <Card.Container key={product.id}>
-          <Card.Image src={product.image} />
-          <Card.Title text={product.title} />
-          <Card.Price price={product.price} />
+      <ul>
+        {data?.map((product) => (
+          <li key={product.id}>
+            <Card.Container>
+              <Card.Image src={product.image} />
+              <Card.Title text={product.title} />
+              <Card.Price price={product.price} />
 
-          <Button.Container>
-            <Button.Icon>
-              <MdOutlineAddShoppingCart size={16} color="#ffff" />0
-            </Button.Icon>
-            ADICIONAR AO CARRINHO
-          </Button.Container>
-        </Card.Container>
-      ))}
+              <Button.Container
+                variant={cartItemsAmount[product.id] ? "success" : "default"}
+                onClick={() => handleAddProduct(product.id)}
+              >
+                <Button.Icon>
+                  <MdOutlineAddShoppingCart size={16} color="#ffff" />
+                  {cartItemsAmount[product.id] || 0}
+                </Button.Icon>
+                ADICIONAR AO CARRINHO
+              </Button.Container>
+            </Card.Container>
+          </li>
+        ))}
+      </ul>
     </Container>
   );
 }
