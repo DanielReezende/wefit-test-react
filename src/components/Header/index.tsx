3b1e3fe8dfc9815446b90150ccb82adfb887ac1c
Daniel Rezende <daniel.zende@gmail.com>
2023-12-09 02:44:39 -0300

    feat: Integration with the useCart hook

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 import { Container, CarLink } from "./styles";
 import { MdShoppingBasket } from "react-icons/md";
-
+import { useCart } from "@/hooks/useCart";
 
 export function Header() {
+  const { cart } = useCart();
+  const cartSize = cart.length;
+
   return (
     <Container>
       <Link to="/">
@@ -14,7 +17,9 @@ export function Header() {
         <CarLink to="/cart">
           <div>
             <strong>Meu Carrinho</strong>
-            <span>0 itens</span>
+            <span>
+              {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
+            </span>
           </div>
 
           <MdShoppingBasket color="#fff" size={32} />
